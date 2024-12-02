ff461beb694dde7af1fb962c2a88046315f42af4
Nguyen Dinh Thuan <thuan.nguyendinh@hust.edu.vn>
2024-12-02 08:45:13 +0700

    Add project files.

---
+++ WebAPIIoT/Controllers/IoTController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIIoT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IoTController : Controller
+    {
+        IoTWebDataContext _context;
+        public IoTController(IoTWebDataContext context) { 
+            _context = context;
+       
+        }
+        [HttpGet("GetDevices")]
+        public IActionResult GetDevices()
+        {
+            var data = _context.Devices.ToList();
+            return Ok(data);
+        }
+    }
+    public class Device
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ WebAPIIoT/IoTWebDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIIoT.Controllers;
+
+namespace WebAPIIoT
+{
+    public class IoTWebDataContext:DbContext
+    {
+        public DbSet<Device> Devices { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS01;Database=IotDB;User ID=iotweb;TrustServerCertificate=True;Password=admin@123;");
+            }
+        }
+    }
+}
